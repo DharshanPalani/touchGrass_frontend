cff5d08170a14e9f2cd929a8c7b2f94cb04e8969
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-03 21:05:30 +0530

    Added user search func with the backend api and also made it show edit option
    Rough rn but should make it usable later

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Auth/Login";
 import Register from "./pages/Auth/Register";
+import Profile from "./profile/Profile";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/profile/:username" element={<Profile />} />
     </Routes>
   );
 }
---
+++ src/profile/Profile.tsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import { api } from "../utils/api";
+import { useParams } from "react-router-dom";
+
+type UserData = {
+  username: string;
+  canEdit: boolean;
+};
+
+function Profile() {
+  const { username } = useParams();
+  const [profileData, setProfileData] = useState<UserData | null>(null);
+
+  useEffect(() => {
+    api.get(`/user/${username}`, { withCredentials: true }).then((res: any) => {
+      setProfileData(res.data);
+    });
+  }, [username]);
+
+  return (
+    <div>
+      Profile: {profileData?.username}{" "}
+      {profileData?.canEdit ? (
+        <button>Edit</button>
+      ) : (
+        <p>Nah fam this ain't your profile</p>
+      )}
+    </div>
+  );
+}
+
+export default Profile;
