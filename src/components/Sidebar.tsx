310a1d784c34d868b04e1060b45a8d2d26753113
DharshanPalani <dharshanpalani.2006@gmail.com>
2025-08-21 21:09:54 +0530

    added so clicking my profile will navigate to the user profile

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,4 +1,15 @@
+import { api } from "../utils/api";
+import { useNavigate } from "react-router-dom";
+
 function Sidebar() {
+  const navigator = useNavigate();
+
+  const handleMyProfile = () => {
+    api.get("/me", { withCredentials: true }).then((res: any) => {
+      navigator("/" + res.data.user.username);
+    });
+  };
+
   return (
     <aside className="h-screen w-56 bg-green-700 text-white font-sans">
       <div className="flex h-full flex-col items-center justify-center space-y-6 text-base font-bold">
@@ -8,9 +19,9 @@ function Sidebar() {
         <a href="/settings" className="hover:underline">
           My Settings
         </a>
-        <a href="/profile" className="hover:underline">
+        <button onClick={handleMyProfile} className="hover:underline">
           My Profile
-        </a>
+        </button>
         <a href="/logout" className="hover:underline">
           Logout
         </a>
